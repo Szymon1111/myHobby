6b623531c338d05efaf3eac9c0294350d2eda042
Szymon1111 <40290583+Szymon1111@users.noreply.github.com>
2019-02-28 23:00:33 +0100

    animate menu opener

--- js/main.js
+++ js/main.js
@@ -5,4 +5,5 @@ document.querySelector('.menu-opener').addEventListener('click',function(){
 document.querySelector('.elements').addEventListener('click',function(){
 if(document.querySelector('.main-menu').classList.contains('opened'))
 	document.querySelector('.main-menu').classList.toggle("opened")
+
 },false);
