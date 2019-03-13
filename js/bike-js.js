a86a3394eabd7c67ca0f6d3b2cecafbc659f8611
Szymon1111 <40290583+Szymon1111@users.noreply.github.com>
2019-03-14 00:08:50 +0100

    foo

--- js/bike-js.js
+++ js/bike-js.js
@@ -18,8 +18,6 @@ function foo(){
     height = limitDown(height,0);
     
     headerBackground.style.height = height+'px';
-    
-    console.log(height);
 }
 function map(n, start1, start2, stop1, stop2) {
   const curr = start2 - start1;
@@ -41,3 +39,26 @@ $(".htbike-img").click(function() {
 $(".fullbike-img").click(function() {
   $.scrollTo('.fullbike', 1000);
 });
+$(".fat-bike-img").click(function() {
+  $.scrollTo('.fat-bike', 1000);
+});
+$(".up-arrow").click(function() {
+  $.scrollTo(0, 1000);
+});
+
+let imgTableHt = ['hornet.jpg','xc-bike.jpg','commencal.jpg'];
+let imgTableFull = ['fuzz.jpg','snabb.jpg','moon.png'];
+let imgTableFat = ['fatbike1.jpg','fatbike2.jpg','fatbike3.jpg'];
+let i = 0;
+
+window.setInterval(function(){
+  i++;
+  if(i>2){
+    i=0;
+  }
+
+  document.querySelector('.section-img').style.backgroundImage = 'url(img/'+imgTableHt[i]+')';
+  document.querySelector('.section-img-full').style.backgroundImage = 'url(img/'+imgTableFull[i]+')';
+  document.querySelector('.section-img-fat').style.backgroundImage = 'url(img/'+imgTableFat[i]+')';
+
+},6000);
